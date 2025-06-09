6c7de126fe70de3964cfe41d20f41ff59d9bba16
RAYUDU SAIPRASANTH <psai9396@gmail.com>
2025-06-09 14:33:25 +0530

    init

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749456980|822230741';
+const CACHE_VERSION = '1749459735|3577450574';
 /** @type {string} */
 const CACHE_PREFIX = 'King's Adventure-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
