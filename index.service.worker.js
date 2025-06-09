b3647a4c68f28f49ecb33474e3b1efd90ce61b9f
RAYUDU SAIPRASANTH <psai9396@gmail.com>
2025-06-09 13:55:20 +0530

    init

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749454527|4892315081';
+const CACHE_VERSION = '1749456980|822230741';
 /** @type {string} */
 const CACHE_PREFIX = 'King's Adventure-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scenes/mobilecontrols.gd
+++ scenes/mobilecontrols.gd
@@ -1,7 +1,29 @@
 extends Node2D
 
-var mobilecontrols: Node2D
+var mobileControls: Node2D
 
 func _ready() -> void:
-	mobilecontrols = $"."
-	mobilecontrols.visible = OS.get_name() == "Android" or OS.get_name() == "iOS"
+	mobileControls = get_node(".") 
+	mobileControls.visible = is_mobile_device()
+
+func is_mobile_device() -> bool:
+	var os_name = OS.get_name()
+
+	# Native mobile platforms
+	if os_name == "Android" or os_name == "iOS":
+		return true
+
+	# For HTML5 (GitHub Pages, browsers)
+	if os_name == "HTML5":
+		if Engine.has_singleton("JavaScript"):
+			var js = Engine.get_singleton("JavaScript")
+			# Detect common mobile user agents
+			var result = js.eval("""
+				(function() {
+					return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+						.test(navigator.userAgent);
+				})()
+			""", true)
+			return result == true
+
+	return false
